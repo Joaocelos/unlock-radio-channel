1927574d898a873bbfe9c0dcad3c8ae2792a74eb
João Celos <76658833+Joaocelos@users.noreply.github.com>
2023-04-04 17:01:35 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Celos'
+description 'This script will spawn the chosen car at the chosen location when it starts'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
